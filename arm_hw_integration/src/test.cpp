83da41e354710499a1d3512309526fc6071873be
shivrar <s_ram_dhanie@hotmail.com>
2018-08-03 17:31:53 -0400

    Merge remote-tracking branch 'origin/hw_integration' into hw_integration
    # Conflicts:
    #	arm_hw_integration/CMakeLists.txt
    #	arm_hw_integration/package.xml
    #	arm_hw_integration/src/test.cpp

--- arm_hw_integration/src/test.cpp
+++ arm_hw_integration/src/test.cpp
@@ -1,8 +1,10 @@
 #include <termios.h>
 #include <unistd.h>
-#include "serial/serial.h"
 #include <ros/ros.h>
 #include <vector>
+#include <iostream>
+#include <cstdio>
+#include "serial/serial.h"
 
 //TODO simple test using the termios serial or serial package interface to get basic motion pragmatically
 
@@ -21,11 +23,60 @@ void enumerate_ports()
 	}
 }
 
+
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "arm_hw_integration_node");
 
+    ros::NodeHandle nh;
+
     enumerate_ports();
+
+    std::string port = "/dev/ttyUSB0";
+
+    unsigned long baud = 9600;
+
+    serial::Timeout timeout = serial::Timeout::simpleTimeout(1000);
+
+    try
+    {
+      serial::Serial test_serial(port, baud, timeout);
+
+      if (test_serial.isOpen())
+      {
+        ROS_INFO("Successful connection");
+
+        std::string command =  "​​0 P1500 T2000";
+        std::string command2 = "​​#1 P1500 T2000";
+        std::string command3 = "​​#2 P1500 T2000";
+        std::string command4 = "​​# P1500 T2000";
+        std::string command5 = "​​#0 P1500 T2000";
+        std::string command6 = "​​#0 P1500 T2000";
+
+
+        test_serial.write(command + "\r");
+
+        test_serial.write("Q\r");
+
+//        usleep(500);
+
+        std::string result = test_serial.read(command.length()+1);
+
+        ROS_INFO("Command sent %s, Result received %s", command.c_str(), result.c_str());
+      }
+      else
+      {
+        ROS_WARN("FAILED connection");
+      }
+    }
+
+    catch (serial::IOException& e)
+    {
+      ROS_ERROR("serial exception: %s, thrown, serial port does not exist", e.what());
+      ros::shutdown();
+    }
+
 
     ros::spin();
 
